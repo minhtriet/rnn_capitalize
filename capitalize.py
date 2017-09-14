2f0adb43e8d5453fe64f329f21f48a3a301bf623
/media/remote_home/mtriet <you@example.com>
2017-09-14 18:14:56 +0200

    fix bugs on input

--- capitalize.py
+++ capitalize.py
@@ -10,8 +10,9 @@
 #     W
 
 import numpy as np
+import pdb
 
-VOCAB_SIZE = 52
+VOCAB_SIZE = 26 
 HIDDEN_NEURON_SIZE = 20
 UNROLL_LENGTH = 50
 CLASSES = 4
@@ -20,18 +21,17 @@ class RNN:
     def __init__(self):
         self.w_xh = np.random.rand(HIDDEN_NEURON_SIZE, VOCAB_SIZE) 
         self.w_hh = np.random.rand(HIDDEN_NEURON_SIZE, HIDDEN_NEURON_SIZE)
-        self.w_hy = np.random.rand(VOCAB_SIZE, HIDDEN_NEURON_SIZE)  # TODO but why HIDDEN_NEURON_SIZE
+        self.w_hy = np.random.rand(HIDDEN_NEURON_SIZE, CLASSES)  # TODO but why HIDDEN_NEURON_SIZE
         self.b_hh = np.random.rand(HIDDEN_NEURON_SIZE, 1)
         self.b_hy = np.random.rand(VOCAB_SIZE, 1)
+        self.h = np.zeros([VOCAB_SIZE, HIDDEN_NEURON_SIZE])
         self.loss = 0
 
-
-    def step(x, target):
-        h = np.tanh(np.dot(self.w_hh, h) + np.dot(self.w_xh, x))
-        y = np.dot(self.w_hy, h)
+    def step(self, x, target):
+        self.h = np.tanh(np.dot(self.h, self.w_hh) + np.dot(x, self.w_xh))
+        y = np.dot(self.h, self.w_hy)
         predict = softmax(y)
         loss = -np.log(target * predict)
-        
         # backward
         dL_o = 1 - predict 
         return loss 
@@ -41,18 +41,16 @@ class RNN:
         return x / e_x.sum()
 
 
-data = open('input.txt', 'r').read() # should be simple plain text file
-result = open('result.txt', 'r').read()
-chars = list(set(data))
-data_size, vocab_size = len(data), len(chars)
-print 'data has %d characters, %d unique.' % (data_size, vocab_size)
-char_to_ix = { ch:i for i,ch in enumerate(chars) }
-ix_to_char = { i:ch for i,ch in enumerate(chars) }
-
+data = open('input.txt', 'r').readlines() # should be simple plain text file
+result = open('result.txt', 'r').readlines()
+char_to_ix = dict(zip(string.ascii_lowercase, range(1,27))) 
 rnn = RNN()
-for i in data:              # TODO why while true?
-    onehot_encode = np.zeros([len(i), VOCAB_SIZE])
-    for index, j in enumerate(i):
+for index_i, sentence in enumerate(data):              # TODO why while true?
+    print "Step %d" % index_i
+    sentence = sentence.strip()
+    onehot_encode = np.zeros([len(sentence), VOCAB_SIZE])
+    pdb.set_trace() 
+    for index, j in enumerate(sentence):
         onehot_encode[index][ char_to_ix[j] ] = 1
-    loss = rnn.step(onehot_encode)
+    loss = rnn.step(onehot_encode, result[index_i])
 
